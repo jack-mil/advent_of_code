3e84fffea6f7b25cfc4180d289e06545e10fea2c
jack-mil.thonkpad <62065280+jack-mil@users.noreply.github.com>
2020-12-01 18:18:58 -0500

    Complete day 1

---
+++ 1/solution.py
@@ -0,0 +1,33 @@
+
+import os
+
+'''
+Read some numarical input from a file.
+Determine the product of the 2 (or 3) entries that *sum* to 2020
+'''
+
+
+def part1() -> int:
+    for n in nums:
+        if 2020 - n in nums:
+            return n * (2020-n)
+    return 0
+
+
+def part2() -> int:
+    for n in nums:
+        for m in nums:
+            if 2020 - n - m in nums:
+                return n * m * (2020 - n - m)
+    return 0
+
+
+if __name__ == "__main__":
+    me = os.path.dirname(__file__)
+
+    with open(os.path.join(me, "input.txt")) as file:
+        nums = set(map(int, file))
+
+    print("Part 1:", part1())
+
+    print("Part 2:", part2())
---
+++ README.md
@@ -0,0 +1,3 @@
+# 2020 Advent of Code!
+
+These are my Python 3 (maybe others languages as well) solutions to 2020 [Advent of Code](https://adventofcode.com/)
