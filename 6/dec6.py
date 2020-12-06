37c6bd7a1ee5685798ab5def88abd7f8161d35e8
jack-mil.thonkpad <62065280+jack-mil@users.noreply.github.com>
2020-12-06 16:10:45 -0500

    Complete day 6

---
+++ 6/dec6.py
@@ -0,0 +1,34 @@
+import os
+from typing import List
+
+
+def part1(answers: List[str]) -> int:
+    count = 0
+    unique = set()
+
+    # Split at blank newlines
+    for group in answers:
+        unique.update(*group.split())
+        count += len(unique)
+        unique.clear()
+    return count
+
+
+def part2(answers: List[str]) -> int:
+    count = 0
+    for group in answers:
+        # Find the intersection of each person's answers per group
+        group_answers = [set(a) for a in group.split()]
+        count += len(set.intersection(*group_answers))
+    return count
+
+
+if __name__ == "__main__":
+    me = os.path.dirname(__file__)
+
+    with open(os.path.join(me, "input.txt")) as file:
+        # Split input on double newlines into groups
+        answers = file.read().split('\n\n')
+
+    print("Part 1:", part1(answers))
+    print("Part 2:", part2(answers))
--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 These are my Python 3 (maybe others languages as well) solutions to 2020 [Advent of Code](https://adventofcode.com/)
 
+They're not the most optimized or "correct" solutions I am sure, but I had fun. 
+
+These solutions use type annotation, as I wanted to try using that feature in Python for the first time.
+
 - [Dec 1](https://adventofcode.com/2020/day/1)
 - [Dec 2](https://adventofcode.com/2020/day/2)
 - [Dec 3](https://adventofcode.com/2020/day/3)
 - [Dec 4](https://adventofcode.com/2020/day/4)
 - [Dec 5](https://adventofcode.com/2020/day/5)
+- [Dec 6](https://adventofcode.com/2020/day/6)
