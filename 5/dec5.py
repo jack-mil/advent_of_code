2a8fc9b5e76611c00bb08cd34697d76af81b1840
jack-mil.thonkpad <62065280+jack-mil@users.noreply.github.com>
2020-12-14 15:34:16 -0500

    comment Day 5

--- 5/dec5.py
+++ 5/dec5.py
@@ -5,15 +5,10 @@ from typing import Iterator, Optional, List
 def get_id(seat: str) -> int:
     # This puzzle can be solved by interpreting the string code
     # as a binary number. The seat id is just the string with (F=0, B=1, L=0, R=1)
-    chars = {'L': '0',
-             'F': '0',
-             'R': '1',
-             'B': '1'}
-
+    chars = {'L': '0', 'F': '0', 'R': '1', 'B': '1'}
     for a, b in chars.items():
         seat = seat.replace(a, b)
-
-    # Convert from the 1-0 string to binary (base 2)
+    # Convert binary string to integer (base 2)
     return int(seat, 2)
 
 
@@ -24,14 +19,14 @@ def parse_seats(passes: Iterator[str]) -> List[int]:
 
 
 def part1(ids: List[int]) -> int:
-    # Part 1 is simple the maximum of all ids
+    # Part 1 is the max of all ids
     return max(ids)
 
 
 def part2(ids: List[int]) -> Optional[int]:
     # Find the sum of all theoretical ids in range [min, max]
-    # The missing seat will be the difference between theoretical sum
-    # and actual sum
+    # The missing seat will be the difference
+    # between theoretical sum and actual sum
     return sum(range(min(ids), max(ids)+1)) - sum(ids)
 
 
